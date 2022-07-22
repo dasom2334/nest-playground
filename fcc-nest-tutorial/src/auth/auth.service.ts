05ef759955b58533b9f211dd96bcc7b645a372ba
dasom <dasom2334@gmail.com>
2022-07-23 00:43:27 +0900

    git init

---
+++ fcc-nest-tutorial/src/auth/auth.service.ts
@@ -0,0 +1,40 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon2 from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    const hash = await argon2.hash(dto.password);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        select: {
+          id: true,
+          email: true,
+          createdAt: true,
+        },
+      });
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials Taken.');
+        }
+        // console.log(error);
+      }
+      throw error;
+    }
+    // delete user.hash;
+  }
+
+  signin() {
+    return { msg: `I am signed in` };
+  }
+}
---
+++ fcc-nest-tutorial/src/prisma/prisma.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: 'postgresql://postgres:12@localhost:5434/nest?schema=public',
+        },
+      },
+    });
+  }
+}
